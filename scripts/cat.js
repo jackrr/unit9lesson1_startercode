166c6b8589592bb1411a5ca6bf1988d08da01140
Jack Ratner <jackratner@gmail.com>
2016-02-25 12:38:43 +0000

    use local images in case of blocked giphy api

--- scripts/cat.js
+++ scripts/cat.js
@@ -1,5 +1,14 @@
 var giphySearchUrl = "https://api.giphy.com/v1/gifs/search?q=cat&api_key=dc6zaTOxFJmzC"
 
+var localGifUrl = function(str){
+    return './images/'+str+'.gif'
+}
+
+var renderLocal = function(){
+    var url = localGifUrl('cat')
+    $('img#gif').attr('src', url)
+}
+
 var render = function(data){
     $('img#gif').attr('src', data.data[0].images.original.url)
 }
@@ -8,4 +17,4 @@ var load = function(){
     $.get(giphySearchUrl, render)
 }
 
-$(document).ready(load)
+$(document).ready(renderLocal)
--- scripts/gif.js
+++ scripts/gif.js
@@ -2,16 +2,29 @@ var giphySearchUrl = function(str){
     return "https://api.giphy.com/v1/gifs/search?q="+str+"&api_key=dc6zaTOxFJmzC"
 }
 
+var localGifUrl = function(str){
+    return '../images/'+str+'.gif'
+}
+
+var renderLocal = function(){
+    var url = localGifUrl(getMusicianName())
+    $('img#gif').attr('src', url)
+}
+
+var getMusicianName = function(){
+    var pName = window.location.pathname;
+    var musician = pName.split('/').pop()
+    return musician.match(/^\w+/)[0]
+}
+
 
 var render = function(data){
     $('img#gif').attr('src', data.data[0].images.original.url)
 }
 
 var load = function(){
-    var pName = window.location.pathname;
-    var musician = pName.split('/').pop()
-    musician = musician.match(/^\w+/)[0]
+    var musician = getMusicianName()
     $.get(giphySearchUrl(musician), render)
 }
 
-$(document).ready(load)
+$(document).ready(renderLocal)
